5a56eec878d84e275feb5fe29ce3d2f314029102
Christine Frank and sofia giordano <sofia@>
2019-09-30 11:53:50 -0700

    successfullt run tests to transalte e and o

--- LeetSpeak.Tests/ModelTests/LeetSpeak.Tests.cs
+++ LeetSpeak.Tests/ModelTests/LeetSpeak.Tests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using LeetSpeak;
 
 namespace LeetSpeak.Tests
@@ -7,10 +8,24 @@ namespace LeetSpeak.Tests
     public class LeetSpeakTest
     {
         [TestMethod]
-        public void Translate_IgnoreNumbers_True()
+        public void Translate_IgnoreNumbers_SameString()
         {
             LeetSpeak leet = new LeetSpeak();
             Assert.AreEqual("h3ll0", leet.Translate("h3ll0"));
+        }
+
+        [TestMethod]
+        public void Translate_ReplaceE_EReplaced()
+        {
+            LeetSpeak leet = new LeetSpeak();
+            Assert.AreEqual("h3ll0", leet.Translate("hell0"));
+        }
+
+        [TestMethod]
+        public void Translate_ReplaceO_OReplaced()
+        {
+            LeetSpeak leet = new LeetSpeak();
+            Assert.AreEqual("h3ll0", leet.Translate("hello"));
         }
     }
 }
